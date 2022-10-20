6813d617ffc08278d7193e4e1ff647333372ade6
Enzo Rocha <enzo.gr16@aluno.ifsc.edu.br>
2022-10-19 22:00:25 -0300

    commit pratica01

---
+++ pratica01/src/pratica01/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Enzo Gabriel da Rocha - Discente do Curso Técnico em Informático IFSC Câmpus Gaspar
+ * @data 19/10/2022
+ **/
+
+package pratica01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
